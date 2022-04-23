9b1e1d2ca236b96fcff48f7daef45f9c06f2ea4c
Javier Garcia <javier.garcia.d@gmail.com>
2022-04-23 12:41:34 -0500

    check reboot function added

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,14 @@
+
 #!python
 
+
+
+def check_reboot():
+    """Returns True if a reboot is pending on a Linux machine
+    """
+    return os.path.exist("/run/reboot-required")
+
+    
 def main():
     print("Hello!")
 
